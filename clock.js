e6a8faba21b87a7dafd8c414fddd0fa369885c7f
Lauren Davis <londond9218@gmail.com>
2015-09-30 16:27:50 -0600

    added sound and working on animation

---
+++ clock.js
@@ -0,0 +1,57 @@
+
+ var myModule = angular.module("myApp", []);
+
+myModule.controller("counterCtrl",['$scope','$timeout', function($scope,$timeout){
+
+
+
+$scope.sec = '0'+0;
+$scope.min = 20;
+$scope.counter = $scope.min * 60;
+
+var stopped;
+var audio = new Audio('bell.wav');
+console.log(parseInt($scope.counter))
+
+
+
+$scope.enterTime = function() {
+  $scope.counter = $scope.min * 60;
+  $scope.sec = '0'+0
+}
+ 
+$scope.countdown = function() { 
+    $scope.durationDisappear = true;
+    stopped = $timeout(function() {
+       console.log($scope.counter);
+     $scope.counter--; 
+     $scope.countdown();   
+    }, 1000);
+    $scope.min = (($scope.counter / 60)-($scope.counter%60/60))
+    if ($scope.min < 10) { $scope.min = '0'+$scope.min}
+    $scope.sec = $scope.counter % 60
+    if ($scope.sec < 10) { $scope.sec = '0'+$scope.sec}
+
+    if ($scope.counter == 0) {
+      $timeout.cancel(stopped) 
+      audio.play(); 
+    }
+
+  };
+
+$scope.stop = function(){
+  $scope.counter = 0;
+  $scope.min = '0'+0;
+  $scope.sec = '0'+0;
+  $timeout.cancel(stopped);
+  $scope.durationDisappear = false;
+}
+    
+$scope.pause = function(){
+   $timeout.cancel(stopped);
+    
+    } 
+
+
+}]);
+
--- main.js
+++ main.js
@@ -1,7 +1,59 @@
-angular.module('vlApp', []); 
+
+// Keypad Buttons 
+function number_write(x)
+{
+  var text_box = document.getElementById("number");
+  if(x>=0 && x<=9)
+  {
+    if(isNaN(text_box.value))
+    	text_box.value = 0;
+ 	text_box.value = (text_box.value * 10)+x;
+  }
+}
+
+function number_clear()
+{
+  document.getElementById("number").value = "00:00";
+}
+
+
+
+
+// Audio Controls 
+function playSound1(){
+  var audio = document.getElementById("audio1");
+       audio.play();
+}
+function playSound2(){
+  var audio = document.getElementById("audio2");
+       audio.play();
+}
+
+
+// Open Door
+function openDoor(field) {
+    var y = $(field).find(".big-box");
+    var x = y.attr("class");
+    if (y.hasClass("back")) {
+        y.removeClass("back");
+    }
+    else {
+        $(".big-box").removeClass("back");
+        y.addClass("back");
+    }
+}
+
+angular.module('myApp', []); 
 
 var mainControllerFunc = function($scope) { 
+	$scope.openClick = false; 
+
+	$scope.openMicro = function(event) {
+		console.log(event); 
+		// if ($scope.openClick === !$scope.openClick)
+		openDoor(event);
+	}
 	
 }
 
-angular.module('vlApp').controller('mainController', ['$scope', mainControllerFunc]); 
+angular.module('myApp').controller('mainController', ['$scope', mainControllerFunc]); 
